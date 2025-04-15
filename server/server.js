dd46ce9bff943cac28af8211c9ece17e1e7205ee
kukusha-yon <yonatanastaf88@gmail.com>
2025-04-15 13:40:50 -0500

    vercel

--- server/server.js
+++ server/server.js
@@ -95,13 +95,13 @@ app.use(errorHandler);
 initializeSettings();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// });
 
 // Start server
 const PORT = process.env.PORT || 5001;
