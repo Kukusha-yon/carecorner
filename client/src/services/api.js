bc5e7b930607c79f0c94c3d37898eaaa53574eac
kukusha-yon <yonatanastaf88@gmail.com>
2025-04-15 23:13:14 -0500

    index to server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://carecorner-az5h51aua-yonatans-projects-2f1159da.vercel.app/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://carecorner-phi.vercel.app/';
 
 // Define public routes that don't require authentication
 const publicRoutes = [
