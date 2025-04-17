0a2b997fd416ca97ef1cdf8f153492b99e3ec626
kukusha-yon <yonatanastaf88@gmail.com>
2025-04-17 01:14:51 -0500

    updated 56

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5001',
-    'https://carecorner-phi.vercel.app'
+    'https://carecorner-bl2n-9thaviglq-yonatans-projects-2f1159da.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
